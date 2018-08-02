cc26afba014a37defffc7d4b7d86d7db6876b869
Iona Macbeth <ionalmacbeth@gmail.com>
2018-08-02 15:01:38 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+const form = document.querySelector('#new-item-form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const bookslist = document.querySelector('ul');
+  const newBook = document.createElement('li');
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const category = event.target.category.value; 
+
+    newBook.textContent = `${title} ${author} ${category}`
+
+  bookslist.appendChild(newBook);
+
+
+})
+
+
+
+})
